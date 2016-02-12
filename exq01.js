446ed3a625847683a7400332610e036dd051db87
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-02-12 05:45:42 +0100

    2016-02-12 initial commit, types and numbers

---
+++ exq01.js
@@ -0,0 +1,14 @@
+// autor nie wymienia null i sumbol (ES6) jako typoów (prymitywnych)
+// wymienia za to obiekt i funkcję
+
+// notacja liczby z wykładnikiem
+var num = 2.987e4;
+
+// specjalne wartości uważane za wartości liczbowe: Infinity, -Infinity, NaN
+
+// infinity
+console.log(Infinity - 1);
+
+// NaN - wynik operacji, która nie dane precyzyjnego, znaczącego wyniku
+console.log(0 / 0);
+console.log(Infinity - Infinity);
